3d7ad3ccd33a467141e50aebf38f941d3f21c814
ghamguiamine <ghamguiamine6868@gmail.com>
2024-06-07 14:29:20 +0100

    first commit

---
+++ x3d_infrences.py
@@ -0,0 +1,27 @@
+def prediction(model, liste_frame):
+    tensor_liste = [torch.from_numpy(arr) for arr in liste_frame]
+    torch_tensor = torch.stack(tensor_liste)
+    inputs=torch_tensor.permute(3, 0, 1, 2)
+    i=dict()
+    i["video"]=inputs
+    video_data = transform(i)
+    
+    inputs = video_data["video"].to(device)
+
+    start_time = time.time()
+    # Make predictions
+    preds = model(inputs[None, ...])
+    
+    # Apply softmax and get predicted classes
+    post_act = torch.nn.Softmax(dim=1)
+    
+    preds = post_act(preds)
+    
+    pred_classes = preds.topk(k=1).indices[0]
+    end_time = time.time()
+    execution_time = end_time - start_time
+    fps=10/execution_time
+    print(preds.topk(k=1))
+    # Map predicted classes to label names
+    pred_class_names = [kinetics_id_to_classname[int(i)] for i in pred_classes]
+    return pred_class_names[0],fps
